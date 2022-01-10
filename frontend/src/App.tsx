80b1c15e04387408408ab48fe7dce746be167dc2
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2022-01-10 15:32:04 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
-import Navbar from "./components/Navbar";
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
-
-
+    <BrowserRouter>
+      <Navbar /> {/* O componente Navbar está fora da tag <Routes>, para ser exibido em todas as páginas */}
+      <Routes>
+        <Route path="/" element={<Listing />} /> {/*Abre a página Listing na home*/}
+        <Route path="/form"> 
+          <Route path=":movieId" element={<Form />} /> {/* Abre a página Form na URI /form/<id do filme>*/}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
